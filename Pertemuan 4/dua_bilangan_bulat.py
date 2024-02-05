55acf8b96a9c5f5e17e73912c1ef99663ad203b0
Dharma azis <140993895+DharmaAzis@users.noreply.github.com>
2024-02-04 18:25:22 -0800

    Add files via upload

---
+++ Pertemuan 4/dua_bilangan_bulat.py
@@ -0,0 +1,9 @@
+x = 4
+y = 2
+
+if ( x >  y):
+    print("Bilangan pertama lebih besar dari pada bilangan kedua")
+elif ( x < y):
+    print("Bilangan kedua lebih besar dari pada bilangan pertama")
+else:
+    print("bilangan tersebut sama")
