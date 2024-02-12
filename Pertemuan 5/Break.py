203191ac8d60dd830e071afe699312f523cfc4f7
Dharma azis <140993895+DharmaAzis@users.noreply.github.com>
2024-02-11 18:44:15 -0800

    Add files via upload

---
+++ Pertemuan 5/Break.py
@@ -0,0 +1,9 @@
+print("\nLOOPING IN PYTHON")
+print("--------------------")
+a = 0
+b = float(input("Masukan angka anda : "))
+while a < b:# a < b adalah syarat
+    print(a)
+    if a == 5: # Seleksi Kondisi
+        break # Break Point 
+    a += 1 # increment
---
+++ Pertemuan 5/continue.py
@@ -0,0 +1,9 @@
+print("\nPenggunaan Continue Pada Looping")
+print("--------------------\n")
+a = 0
+b = float(input("Masukan angka anda : "))
+while a < b: # a < b adalah syarat
+    a += 1 # Increment
+    if a == 5: # Seleksi Kondisi
+        continue # Continue Point
+    print(a)
