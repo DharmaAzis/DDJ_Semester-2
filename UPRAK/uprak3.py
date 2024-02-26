be20e393e0ead1a3fa9d23bb46dd0bb4190758d3
Dharma azis <140993895+DharmaAzis@users.noreply.github.com>
2024-02-25 18:14:11 -0800

    Add files via upload

---
+++ UPRAK/uprak3.py
@@ -0,0 +1,13 @@
+belanja = [
+    {"buah":"apel", "harga":5000*10},
+    {"buah":"nanas", "harga":3000*5},
+    {"buah":"pepaya", "harga":7000*7},
+    {"buah":"pisang", "harga":2000*20},
+    {"buah":"semangka", "harga":15000*1},
+  ]
+
+total_belanjaan = 0
+for item in belanja:
+    total_belanjaan += item["harga"]
+
+print("Total Belanja : ", total_belanjaan)    
